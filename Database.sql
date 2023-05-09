adabd27e5e8479387694e9e15b06c6aec5880f6f
ilyasin hafid <ilyasin7700@gmail.com>
2023-05-09 09:28:42 +0700

    penambahan pilihan framework + database dan persiapan template untuk yang lain

--- Database.sql
+++ Database.sql
@@ -1,6 +1,7 @@
 CREATE DATABASE [Vleko.TemplateProject]
 
-CREATE TABLE dbo.REPOSITORY (
+//#if(Database=="mssql")
+CREATE TABLE Vleko.TemplateProject.dbo.REPOSITORY (
 	ID uniqueidentifier DEFAULT newid() NOT NULL,
 	CODE varchar(100) COLLATE Latin1_General_CI_AS NOT NULL,
 	MODUL varchar(150) COLLATE Latin1_General_CI_AS NOT NULL,
@@ -14,7 +15,7 @@ CREATE TABLE dbo.REPOSITORY (
 	CONSTRAINT PK_REPOSITORY PRIMARY KEY (ID)
 );
 
-CREATE TABLE dbo.[ROLE] (
+CREATE TABLE Vleko.TemplateProject.dbo.[ROLE] (
 	ID varchar(50) COLLATE Latin1_General_CI_AS NOT NULL,
 	NAME varchar(100) COLLATE Latin1_General_CI_AS NOT NULL,
 	ACTIVE bit NOT NULL,
@@ -25,7 +26,7 @@ CREATE TABLE dbo.[ROLE] (
 	CONSTRAINT PK_ROLE PRIMARY KEY (ID)
 );
 
-CREATE TABLE dbo.[USER] (
+CREATE TABLE Vleko.TemplateProject.dbo.[USER] (
 	ID uniqueidentifier DEFAULT newid() NOT NULL,
 	USERNAME nvarchar(80) COLLATE Latin1_General_CI_AS NOT NULL,
 	PASSWORD nvarchar(50) COLLATE Latin1_General_CI_AS NOT NULL,
@@ -43,7 +44,7 @@ CREATE TABLE dbo.[USER] (
 	CONSTRAINT PK_USER PRIMARY KEY (ID)
 );
 
-CREATE TABLE dbo.NOTIFICATION (
+CREATE TABLE Vleko.TemplateProject.dbo.NOTIFICATION (
 	ID uniqueidentifier DEFAULT newid() NOT NULL,
 	ID_USER uniqueidentifier NOT NULL,
 	USER_FULLNAME varchar(150) COLLATE Latin1_General_CI_AS NOT NULL,
@@ -59,7 +60,7 @@ CREATE TABLE dbo.NOTIFICATION (
 	CONSTRAINT NOTIFICATION_FK FOREIGN KEY (ID_USER) REFERENCES [Vleko.TemplateProject].dbo.[USER](ID) ON DELETE CASCADE
 );
 
-CREATE TABLE dbo.USER_ROLE (
+CREATE TABLE Vleko.TemplateProject.dbo.USER_ROLE (
 	ID uniqueidentifier DEFAULT newid() NOT NULL,
 	ID_USER uniqueidentifier NOT NULL,
 	ID_ROLE varchar(50) COLLATE Latin1_General_CI_AS NOT NULL,
@@ -70,12 +71,95 @@ CREATE TABLE dbo.USER_ROLE (
 	CONSTRAINT FK_USER_ROLE_USER FOREIGN KEY (ID_USER) REFERENCES [Vleko.TemplateProject].dbo.[USER](ID) ON DELETE CASCADE
 );
 
-INSERT INTO dbo.[ROLE] (ID,NAME,ACTIVE,CREATE_BY,CREATE_DATE,UPDATE_BY,UPDATE_DATE) VALUES
+INSERT INTO Vleko.TemplateProject.dbo.[ROLE] (ID,NAME,ACTIVE,CREATE_BY,CREATE_DATE,UPDATE_BY,UPDATE_DATE) VALUES
 	 (N'ADM',N'Admin',1,N'SYSTEM',getdate(),N'SYSTEM',getdate()),
 	 (N'MBR',N'Member',1,N'SYSTEM',getdate(),N'SYSTEM',getdate());
 
-INSERT INTO dbo.[USER] (ID,USERNAME,PASSWORD,FULLNAME,MAIL,PHONE_NUMBER,TOKEN,IS_LOCKOUT,ACCESS_FAILED_COUNT,ACTIVE,CREATE_BY,CREATE_DATE,UPDATE_BY,UPDATE_DATE) VALUES
+INSERT INTO Vleko.TemplateProject.dbo.[USER] (ID,USERNAME,PASSWORD,FULLNAME,MAIL,PHONE_NUMBER,TOKEN,IS_LOCKOUT,ACCESS_FAILED_COUNT,ACTIVE,CREATE_BY,CREATE_DATE,UPDATE_BY,UPDATE_DATE) VALUES
 	 (N'491874CE-92C0-4CFD-83B7-F916DF7C219B',N'Admin',N'5F4DCC3B5AA765D61D8327DEB882CF99',N'Administrator',N'admin@mail.com',N'0000000',null,0,0,1,N'SYSTEM',getdate(),N'SYSTEM',getdate());
 
-INSERT INTO dbo.USER_ROLE (ID,ID_USER,ID_ROLE,CREATE_BY,CREATE_DATE) VALUES
+INSERT INTO Vleko.TemplateProject.dbo.USER_ROLE (ID,ID_USER,ID_ROLE,CREATE_BY,CREATE_DATE) VALUES
 	 (N'2DA981E5-B0EE-4630-B755-2A10CA0F15BF',N'491874CE-92C0-4CFD-83B7-F916DF7C219B',N'ADM',N'SYSTEM',getdate());
+//#endif
+
+//#if(Database=="postgre")
+CREATE TABLE Vleko.TemplateProject.public.repository (
+	id uuid NOT NULL DEFAULT gen_random_uuid(),
+	code varchar(100) NOT NULL,
+	modul varchar(150) NOT NULL,
+	file_name varchar(150) NOT NULL,
+	description varchar(1000) NULL,
+	"extension" varchar(20) NOT NULL,
+	mime_type varchar(100) NOT NULL,
+	is_public boolean NOT NULL,
+	create_by varchar(250) NOT NULL DEFAULT 'SYSTEM'::character varying,
+	create_date timestamp NOT NULL DEFAULT now(),
+	CONSTRAINT pk_repository PRIMARY KEY (id)
+);
+
+CREATE TABLE Vleko.TemplateProject.public."role" (
+	id varchar(50) NOT NULL,
+	"name" varchar(100) NOT NULL,
+	active boolean NOT NULL,
+	create_by varchar(250) NOT NULL DEFAULT 'SYSTEM'::character varying,
+	create_date timestamp NOT NULL DEFAULT now(),
+	update_by varchar(250) NULL,
+	update_date timestamp NULL,
+	CONSTRAINT pk_role PRIMARY KEY (id)
+);
+
+CREATE TABLE Vleko.TemplateProject.public."user" (
+	id uuid NOT NULL DEFAULT gen_random_uuid(),
+	username varchar(80) NOT NULL,
+	"password" varchar(50) NOT NULL,
+	fullname varchar(250) NULL,
+	mail varchar(150) NOT NULL,
+	phone_number varchar(50) NULL,
+	"token" varchar(250) NULL,
+	is_lockout boolean NOT NULL,
+	access_failed_count int NOT NULL,
+	active boolean NOT NULL,
+	create_by varchar(250) NOT NULL DEFAULT 'SYSTEM'::character varying,
+	create_date timestamp NOT NULL DEFAULT now(),
+	update_by varchar(250) NULL,
+	update_date timestamp NULL,
+	CONSTRAINT pk_user PRIMARY KEY (id)
+);
+
+CREATE TABLE Vleko.TemplateProject.public.notification (
+	id uuid NOT NULL DEFAULT gen_random_uuid(),
+	id_user uuid NOT NULL,
+	user_fullname varchar(150) NOT NULL,
+	user_mail varchar(150) NULL,
+	user_phone varchar(50) NULL,
+	subject varchar(50) NOT NULL,
+	description varchar(250) NOT NULL,
+	navigation varchar(250) NOT NULL,
+	is_open boolean NOT NULL,
+	create_by varchar(250) NOT NULL,
+	create_date timestamp NOT NULL DEFAULT now(),
+	CONSTRAINT pk_notification PRIMARY KEY (id),
+	CONSTRAINT notification_user_fk FOREIGN KEY (id_user) REFERENCES Vleko.TemplateProject.public."user"(id) ON DELETE CASCADE ON UPDATE RESTRICT
+);
+
+CREATE TABLE Vleko.TemplateProject.public.user_role (
+	id uuid NOT NULL DEFAULT gen_random_uuid(),
+	id_user uuid NOT NULL,
+	id_role varchar(50) NOT NULL,
+	create_by varchar(250) NOT NULL,
+	create_date timestamp NOT NULL DEFAULT now(),
+	CONSTRAINT pk_user_role PRIMARY KEY (ID),
+	CONSTRAINT user_role_role_fk FOREIGN KEY (id_role) REFERENCES Vleko.TemplateProject.public."role"(id) ON DELETE CASCADE ON UPDATE RESTRICT,
+	CONSTRAINT user_role_user_fk FOREIGN KEY (id_user) REFERENCES Vleko.TemplateProject.public."user"(id) ON DELETE CASCADE ON UPDATE RESTRICT
+);
+
+INSERT INTO Vleko.TemplateProject.public."role" (id,"name",active,create_by,create_date,update_by,update_date) VALUES
+	 ('ADM','Admin',true,'SYSTEM',now(),'SYSTEM',now()),
+	 ('MBR','Member',true,'SYSTEM',now(),'SYSTEM',now());
+
+INSERT INTO Vleko.TemplateProject.public."user" (id,username,"password",fullname,mail,phone_number,"token",is_lockout,access_failed_count,active,create_by,create_date,update_by,update_date) VALUES
+	 ('4ef08bb2-7ecd-4402-b2d7-08f6066caf2b','Admin','5F4DCC3B5AA765D61D8327DEB882CF99','Administrator','admin@mail.com','0000000',null,false,0,true,'SYSTEM',now(),'SYSTEM',now());
+
+INSERT INTO public."user_role" (id,id_user,id_role,create_by,create_date) VALUES
+	 (gen_random_uuid(),'4ef08bb2-7ecd-4402-b2d7-08f6066caf2b','ADM','SYSTEM',now());
+//#endif
--- README.md
+++ README.md
@@ -15,9 +15,14 @@ dotnet tool update --global dotnet-ef
 ### 2.Scaffolding 
 setelah mengeksekusi install tool kemudian ketik/copy code berikut :
 ```scaffold
+<!--#if(Database=="postgre") -->
+dotnet ef dbcontext scaffold "Host=localhost;Database=Vleko.TemplateProject;Username=[YOUR_USERNAME];Password=[YOUR_PASSWORD]" Npgsql.EntityFrameworkCore.PostgreSQL --output-dir "..\Vleko.TemplateProject.Data\Model" -c ApplicationDBContext --context-dir "..\Vleko.TemplateProject.Data" --namespace "Vleko.TemplateProject.Data.Model"  --context-namespace "Vleko.TemplateProject.Data" --no-pluralize -f --no-onconfiguring --schema "public"
+<!--#endif -->
+<!--#if(Database=="mssql") -->
 dotnet ef dbcontext scaffold "Data Source=localhost;Initial Catalog=Vleko.TemplateProject;Integrated Security=True" Microsoft.EntityFrameworkCore.SqlServer --output-dir "..\Vleko.TemplateProject.Data\Model" -c ApplicationDBContext --context-dir "..\Vleko.TemplateProject.Data" --namespace "Vleko.TemplateProject.Data.Model" --context-namespace "Vleko.TemplateProject.Data" --no-pluralize -f --no-onconfiguring --schema "dbo"
+<!--#endif -->
 ```
-ganti localhost apabila ingin merubah koneksi ke server yang dituju.
+ganti localhost username dan password apabila ingin merubah koneksi ke server yang dituju.
 
 ### 3.Generated file
 show all files di project Vleko.TemplateProject.Data maka akan terbentuk file *generated" dan didalamnya terdapat backend dan frontend disana tinggal copy paste saja kedalam core untuk kebutuhan table.
